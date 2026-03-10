4a98ab70bd693d2562589308012d72c7d1596f0f
GANESHA N S <ganeshns935@gmail.com>
2026-03-11 01:14:23 +0530

    Update create_s3.sh

--- s3/create_s3.sh
+++ s3/create_s3.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
-set -e
+#!/bin/bash 
 
-BUCKET_NAME=$1
-
-echo "Creating bucket: $BUCKET_NAME"
-
-aws s3 mb s3://$BUCKET_NAME --region us-east-1
-
-echo "Bucket created successfully"
+aws s3 mb $1
